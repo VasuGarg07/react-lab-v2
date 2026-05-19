ff4afd11fcff7ced937214ea2ba284f88a550b8d
Vasu Garg <vasugarg7860@gmail.com>
2026-05-19 23:35:10 +0530

    auth_server microservice

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,24 @@
-import apiClient from "../shared/apiClient";
+import axios from "axios";
+import { CONFIG } from "../shared/config";
 import type { ChangePasswordData, LoginData, RegisterData } from "./auth.types";
 
+const apiClient = axios.create({
+    baseURL: CONFIG.AUTH_URL
+})
+
 export const register = async (data: RegisterData): Promise<void> => {
-    await apiClient.post("/auth/register", data);
+    await apiClient.post("/register", data);
 };
 
 export const login = async (data: LoginData): Promise<{ accessToken: string; refreshToken: string }> => {
-    const response = await apiClient.post("/auth/login", data);
+    const response = await apiClient.post("/login", data);
     return response.data;
 };
 
 export const changePassword = async (data: ChangePasswordData): Promise<void> => {
-    await apiClient.post("/auth/change-password", data);
+    await apiClient.post("/change-password", data);
 };
 
 export const logout = async (refreshToken: string): Promise<void> => {
-    await apiClient.post("/auth/logout", { refreshToken });
+    await apiClient.post("/logout", { refreshToken });
 };
--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,4 +1,5 @@
 export const CONFIG = {
     API_URL: import.meta.env.VITE_API_URL,
+    AUTH_URL: import.meta.env.VITE_AUTH_SERVER_URL,
     IMGBB_API_KEY: import.meta.env.VITE_IMGBB_API_KEY,
 }
